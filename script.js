ab3ecdc9d5b48efb1a52d04bc81af40825ee9522
Hallel <yahalomm@gmail.com>
2019-04-22 13:42:34 +0200

    Это мой первый комит

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+
+console.log('string' * 9);
+let somthing;
+console.log(somthing);
+let persone = {
+    name: 'John',
+    age: 25,
+    isMaried: false
+}
+console.log(persone['name']);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+console.log(arr[0]);
+// alert('Hello World!');
+
+// let answer = confirm('Are you here?');
+
+//let answer = +prompt('Есть ли вам 18?', 'Да');
+
+//console.log(typeof(answer));
+
+//console.log(4 + +' - object')
+
+let incr = 10,
+    decr = 10;
+
+  
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+ 
